ff9928aaa388bae70acef3337fa44f2f7b4c7792
Bishoy Ezzat <92762861+BihSo@users.noreply.github.com>
2024-12-28 14:18:38 +0200

    Add files via upload

---
+++ MasterMindGame/src/main/java/loyalixa/oop/mastermindgame/Main.java
@@ -0,0 +1,27 @@
+package loyalixa.oop.mastermindgame;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("master_mind.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 500);
+        stage.setMinWidth(600);
+        stage.setMinHeight(500);
+        stage.setTitle("MasterMind Game by Loyalixa");
+        stage.getIcons().add(new Image(getClass().getResource("logo.png").toExternalForm()));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
